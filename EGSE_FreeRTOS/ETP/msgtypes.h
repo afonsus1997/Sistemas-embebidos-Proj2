7b67c6ee43f17825a2f80787e16982dd5c328c1e
Afonso Muralha <afonsomuralha@gmail.com>
2019-12-13 15:55:26 +0000

    Changes gpio expander writes to psu set due to hardware bug. Minor changes to msg types

--- EGSE_FreeRTOS/ETP/msgtypes.h
+++ EGSE_FreeRTOS/ETP/msgtypes.h
@@ -118,12 +118,12 @@ typedef enum {
 } ADCVals_e;
 
 typedef enum {
-    GPIO_1, GPIO_2,
-    GPIO_3, GPIO_4,
-    GPIO_5, GPIO_6,
-    GPIO_7, GPIO_8,
-    GPIO_9, GPIO_10,
-    GPIO_11, GPIO_12, GPIO_13,
+    GPIO_0, GPIO_1,
+    GPIO_2, GPIO_3,
+    GPIO_4, GPIO_4,
+    GPIO_6, GPIO_5,
+    GPIO_8, GPIO_9,
+    GPIO_10, GPIO_11, GPIO_12,
 } GPIOVals_e;
 
 /*===========================*/
